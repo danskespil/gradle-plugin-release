451ab8aad0988b0d57fc0906621a1ee166832bc7
Jesper Wermuth <jwermuth@gmail.com>
2018-01-26 16:01:51 +0100

    Add an incrementor to the tag and version

--- src/main/groovy/dk/danskespil/gradle/plugins/release/DSStrategies.groovy
+++ src/main/groovy/dk/danskespil/gradle/plugins/release/DSStrategies.groovy
@@ -4,6 +4,7 @@ import org.ajoberstar.gradle.git.release.opinion.Strategies
 import org.ajoberstar.gradle.git.release.semver.PartialSemVerStrategy
 import org.ajoberstar.gradle.git.release.semver.SemVerStrategy
 
+import static org.ajoberstar.gradle.git.release.semver.StrategyUtil.all
 import static org.ajoberstar.gradle.git.release.semver.StrategyUtil.closure
 
 class DSStrategies {
@@ -11,11 +12,11 @@ class DSStrategies {
             name: 'branch',
             stages: ['branch'] as SortedSet,
             allowDirtyRepo: false,
-            preReleaseStrategy: PreRelease.BRANCH,
+            preReleaseStrategy: all(Strategies.PreRelease.STAGE_FIXED, Strategies.PreRelease.COUNT_INCREMENTED, PreRelease.BRANCH),
             createTag: true
     )
 
-    
+
     static final class PreRelease {
         static final PartialSemVerStrategy BRANCH = closure { state -> state.copyWith(inferredPreRelease: state.currentBranch.name) }
     }
